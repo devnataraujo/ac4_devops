7ef224c2a5a9794658146b1642310351c9eb7c4b
Nathalia de Araujo Santos <2201044@SALAS.AULAS>
2023-04-24 21:31:19 -0300

    alterando ip - 2

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'natju'
 app.config['MYSQL_DATABASE_DB'] = 'teste'
-app.config['MYSQL_DATABASE_HOST'] = '172.18.0.1'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
 
